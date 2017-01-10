7185039f7caf3033c0244ded2c74412cddbcc616
kazuemasaki <kazuemasaki@gmail.com>
2017-01-10 14:34:37 +0900

    fuel関連のパッケージをインストールするchefレシピ

---
+++ vagrant/centos6.6/chef-repo/site-cookbooks/fuelphp/README.md
@@ -0,0 +1,4 @@
+# fuelphp
+
+TODO: Enter the cookbook description here.
+
---
+++ vagrant/centos6.6/chef-repo/site-cookbooks/fuelphp/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: fuelphp
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+bash 'install packages by composer' do
+	cwd '/var/www/practice/'
+	code <<-EOC
+		php composer.phar install
+	EOC
+	creates '/var/www//practice/composer.lock'
+end
+
+bash 'update packages by composer' do
+	cwd '/var/www/practice/'
+	code <<-EOC
+		php composer.phar self-update
+		php composer.phar update
+	EOC
+end
