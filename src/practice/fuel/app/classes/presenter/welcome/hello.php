1ef8bd59e1e6dbaddb9358cffed22d01eb08f2a9
kazuemasaki <kazuemasaki@gmail.com>
2017-01-13 20:28:14 +0900

    revert

--- src/practice/fuel/app/classes/presenter/welcome/hello.php
+++ src/practice/fuel/app/classes/presenter/welcome/hello.php
@@ -9,9 +9,6 @@
 class Presenter_Welcome_Hello extends Presenter
 {
 
-
-	public $_view = 'welcome/hello';
-
 	/**
 	 * Prepare the view data, keeping this in here helps clean up
 	 * the controller.
@@ -20,9 +17,6 @@ class Presenter_Welcome_Hello extends Presenter
 	 */
 	public function view()
 	{
-		// var_dump($this->_view);die;
-		// $this->_view = 'app/views/welcome/hello.php';
-		// $this->_view = 'welcome/hello';
 		$this->name = $this->request()->param('name', 'World');
 	}
 }
