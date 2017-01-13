6c5887c237d660f4d31372f07f3da1f50eb39e65
kazuemasaki <kazuemasaki@gmail.com>
2017-01-13 20:27:46 +0900

    プレゼンタ⇄ビュー間で違う名前を使う方法2

--- src/practice/fuel/app/classes/presenter/welcome/hello.php
+++ src/practice/fuel/app/classes/presenter/welcome/hello.php
@@ -8,6 +8,10 @@
  */
 class Presenter_Welcome_Hello extends Presenter
 {
+
+
+	public $_view = 'welcome/hello';
+
 	/**
 	 * Prepare the view data, keeping this in here helps clean up
 	 * the controller.
@@ -16,6 +20,9 @@ class Presenter_Welcome_Hello extends Presenter
 	 */
 	public function view()
 	{
+		// var_dump($this->_view);die;
+		// $this->_view = 'app/views/welcome/hello.php';
+		// $this->_view = 'welcome/hello';
 		$this->name = $this->request()->param('name', 'World');
 	}
 }
