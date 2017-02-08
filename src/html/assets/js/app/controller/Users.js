6acf59a51f9bd73015b1e04a374800505a5c326b
kazuemasaki <kazuemasaki@gmail.com>
2017-02-08 20:05:12 +0900

    end of ”MVC Architecture” http://docs.sencha.com/extjs/4.2.1/#!/guide/application_architecture

--- src/html/assets/js/app/controller/Users.js
+++ src/html/assets/js/app/controller/Users.js
@@ -2,18 +2,47 @@ Ext.define('AM.controller.Users', {
     extend: 'Ext.app.Controller',
 
     views: [
-        'user.List'
+        'user.List',
+        'user.Edit'
     ],
-    
+
+    stores: [
+        'Users'
+    ],
+
+    models: ['User'],
+
     init: function() {
         this.control({
             'viewport > panel': {
                 render: this.onPanelRendered
-            }
+            },
+            'userlist': {
+                itemdblclick: this.editUser
+            },
+            'useredit button[action=save]': {
+                click: this.updateUser
+            }                
         });
     },
 
     onPanelRendered: function() {
         console.log('The panel was rendered');
-    }
+    },
+    editUser: function(grid, record) {
+        var view = Ext.widget('useredit');
+        view.down('form').loadRecord(record);
+        console.log('Double clicked on ' + record.get('name'));
+    },
+    updateUser: function(button) {
+		var win    = button.up('window'),
+		    form   = win.down('form'),
+		    record = form.getRecord(),
+		    values = form.getValues();
+
+		record.set(values);
+		win.close();    	
+		// synchronize the store after editing the record
+		this.getUsersStore().sync();
+    }    
 });
