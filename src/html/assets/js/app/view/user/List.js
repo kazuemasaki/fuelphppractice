7316ff268443c4ec344916d4516940dcfea7c4c3
kazuemasaki <kazuemasaki@gmail.com>
2017-02-08 20:58:06 +0900

    add Layouts and Containers(http://docs.sencha.com/extjs/4.2.1/#!/guide/layouts_and_containers) first sample code

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -1,16 +1,57 @@
 Ext.define('AM.view.user.List' ,{
-    extend: 'Ext.grid.Panel',
+    // extend: 'Ext.grid.Panel',
+    extend: 'Ext.panel.Panel',
     alias: 'widget.userlist',
 
-    title: 'All Users',
+    renderTo: Ext.getBody(),
+    width: 400,
+    height: 300,
+    title: 'Container Panel',
+    items: [
+        {
+            xtype: 'panel',
+            title: 'Child Panel 1',
+            height: 100,
+            width: '75%'
+        },
+        {
+            xtype: 'panel',
+            title: 'Child Panel 2',
+            height: 100,
+            width: '75%'
+        }
+    ]
 
-    store: 'Users',
-    initComponent: function() {
-        this.columns = [
-            {header: 'Name',  dataIndex: 'name',  flex: 1},
-            {header: 'Email', dataIndex: 'email', flex: 1}
-        ];
+    // title: 'All Users',
 
-        this.callParent(arguments);
-    }
+    // store: 'Users',
+    // initComponent: function() {
+    //     this.columns = [
+    //         {header: 'Name',  dataIndex: 'name',  flex: 1},
+    //         {header: 'Email', dataIndex: 'email', flex: 1}
+    //     ];
+
+    //     this.callParent(arguments);
+    // }
+});
+
+Ext.create('Ext.panel.Panel', {
+    // renderTo: Ext.getBody(),
+    // width: 400,
+    // height: 300,
+    // title: 'Container Panel',
+    // items: [
+    //     {
+    //         xtype: 'panel',
+    //         title: 'Child Panel 1',
+    //         height: 100,
+    //         width: '75%'
+    //     },
+    //     {
+    //         xtype: 'panel',
+    //         title: 'Child Panel 2',
+    //         height: 100,
+    //         width: '75%'
+    //     }
+    // ]
 });
