1cc638c1f23de509a6390e34974e6c1ac66f4874
kazuemasaki <kazuemasaki@gmail.com>
2017-02-27 20:59:38 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/example/layout-browser/layout-browser.html column layout

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -14,32 +14,22 @@ Ext.define('AM.view.user.List' ,{
     renderTo: Ext.getBody(),
     width: 400,
     height: 200,
-    title: 'Card Layout (Wizard)',
-    layout:'card',
-    activeItem: 0,
-    bodyStyle: 'padding:15px',
-    defaults: {border:false},
-    bbar: ['->', {
-        id: 'card-prev',
-        text: '&laquo; Previous',
-        handler: Ext.Function.bind(cardNav, this, [-1]),
-        disabled: true
-    },{
-        id: 'card-next',
-        text: 'Next &raquo;',
-        handler: Ext.Function.bind(cardNav, this, [1])
-    }],
-    items: [{
-        id: 'card-0',
-        html: '<h1>Welcome to the Demo Wizard!</h1><p>Step 1 of 3</p><p>Please click the "Next" button to continue...</p>'
-    },{
-        id: 'card-1',
-        html: '<p>Step 2 of 3</p><p>Almost there.  Please click the "Next" button to continue...</p>'
-    },{
-        id: 'card-2',
-        html: '<h1>Congratulations!</h1><p>Step 3 of 3 - Complete</p>'
-    }]
 
+    layout:'column',
+    items: [{
+        title: 'Width = 25%',
+        columnWidth: .25,
+        html: 'Content'
+    },{
+        title: 'Width = 75%',
+        columnWidth: .75,
+        html: 'Content'
+    },{
+        title: 'Width = 250px',
+        width: 250,
+        html: 'Content'
+    }]
+             
     // title: 'All Users',
 
     // store: 'Users',
