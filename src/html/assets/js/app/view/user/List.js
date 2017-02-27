d01764449f9ac301a50b85e8381027a3272cc24c
kazuemasaki <kazuemasaki@gmail.com>
2017-02-27 20:31:24 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/example/layout-browser/layout-browser.html border layout

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -7,19 +7,45 @@ Ext.define('AM.view.user.List' ,{
     width: 400,
     height: 200,
     title: 'Container Panel',
-    layout: 'anchor',
+
+    layout: 'border',
+    bodyBorder: false,
+    defaults: {
+        collapsible: true,
+        split: true,
+        bodyPadding: 15
+    },
     items: [{
-        title: 'Panel 1',
-        height: 100,
-        anchor: '50%'
+        title: 'Navigation',
+        region:'west',
+        floatable: false,
+        margins: '5 0 0 0',
+        width: 175,
+        minWidth: 100,
+        maxWidth: 250,
+        html: 'Secondary content like navigation links could go here'
     },{
-        title: 'Panel 2',
-        height: 100,
-        anchor: '-10'
+        title: 'heder',
+        region: 'north',
+        height: 150,
+        minHeight: 75,
+        maxHeight: 250,
+        html: 'header content'
     },{
-        title: 'Panel 3',
-        anchor: '-100, -262'
+        title: 'Footer',
+        region: 'south',
+        height: 150,
+        minHeight: 75,
+        maxHeight: 250,
+        html: 'Footer content'
+    },{
+        title: 'Main Content',
+        collapsible: false,
+        region: 'center',
+        margins: '5 0 0 0',
+        html: 'Main PageThis is where the main content would go'
     }]
+
 
     // title: 'All Users',
 
