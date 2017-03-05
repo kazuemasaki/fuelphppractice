971a11167f39150a36f6e0882cee5cbad8cfe302
kazuemasaki <kazuemasaki@gmail.com>
2017-03-05 15:00:22 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/example/layout-browser/layout-browser.html Absolute Layout Form

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -15,30 +15,98 @@ Ext.define('AM.view.user.List' ,{
     width: 400,
     height: 200,
 
-    layout: {
-        type: 'hbox',
-        pack: 'start',
-        align: 'stretch'
-    },
-    defaults: {
-        frame: true
-    },
-    items: [{
-        title: 'Panel 1',
-        flex: 1,
-        margin: '0 0 0 0',
-        html: 'flex : 1'
-    }, {
-        title: 'Panel 2',
-        width: 100,
-        margin: '0 0 0 0',
-        html: 'width : 100'
-    }, {
-        title: 'Panel 3',
-        flex: 2,
-        html: 'flex : 2'
-    }]
-             
+    layout: 'fit',
+    bodyPadding: 15,
+    items: {
+        title: 'New Email',
+        layout: 'fit',
+        frame: true,
+        border: false,
+        items: {
+            xtype: 'form',
+            layout:'absolute',
+            url:'save-form.php',
+            padding: '5 5 0 5',
+            border: false,
+            cls: 'absolute-form-panel-body',
+            
+            defaultType: 'textfield',
+            items: [{
+                x: 0,
+                y: 5,
+                xtype: 'label',
+                text: 'From:'
+            },{
+                x: 55,
+                y: 0,
+                name: 'from',
+                hideLabel: true,
+                anchor:'100%'  // anchor width by %
+            },{
+                x: 0,
+                y: 32,
+                xtype: 'label',
+                text: 'To:'
+            },{
+                x: 55,
+                y: 27,
+                width: 69,
+                xtype: 'button',
+                text: 'Contacts'
+            },{
+                x: 127,
+                y: 27,
+                name: 'to',
+                hideLabel: true,
+                anchor: '100%'  // anchor width by %
+            },{
+                x: 0,
+                y: 59,
+                xtype: 'label',
+                text: 'Subject:'
+            },{
+                x: 55,
+                y: 54,
+                name: 'subject',
+                hideLabel: true,
+                anchor: '100%'  // anchor width by %
+            },{
+                x: 0,
+                y: 81,
+                hideLabel: true,
+                xtype: 'textarea',
+                name: 'msg',
+                anchor: '100% 100%'  // anchor width and height
+            }]
+        },
+
+        dockedItems: [
+            {
+                xtype: 'toolbar',
+                border: false,
+                cls: 'absolute-form-panel-body',
+                items: [
+                    {
+                         text: 'Send',
+                         iconCls: 'icon-send'
+                    },'-',{
+                         text: 'Save',
+                         iconCls: 'icon-save'
+                    },{
+                         text: 'Check Spelling',
+                         iconCls: 'icon-spell'
+                    },'-',{
+                         text: 'Print',
+                         iconCls: 'icon-print'
+                    },'->',{
+                         text: 'Attach a File',
+                         iconCls: 'icon-attach'
+                    }
+                ]
+            }
+        ]
+    }
+
     // title: 'All Users',
 
     // store: 'Users',
