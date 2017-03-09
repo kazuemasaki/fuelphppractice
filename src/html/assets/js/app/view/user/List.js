bbfa2b6c36d7b6c78bdf94decaef2bfcc6b73ab6
kazuemasaki <kazuemasaki@gmail.com>
2017-03-09 20:21:16 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/guide/components sample

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -1,35 +1,13 @@
-var containerPanel = Ext.create('Ext.panel.Panel', {
-    renderTo: Ext.getBody(),
-    width: 400,
-    height: 200,
-    title: 'Container Panel',
-    layout: 'column',
-    suspendLayout: true // Suspend automatic layouts while we do several different things that could trigger a layout on their own
-});
-// Add a couple of child items.  We could add these both at the same time by passing an array to add(),
-// but lets pretend we needed to add them separately for some reason.
-containerPanel.add({
-    xtype: 'panel',
+var childPanel1 = Ext.create('Ext.panel.Panel', {
     title: 'Child Panel 1',
-    height: 100,
-    columnWidth: 0.5
+    html: 'A Panel'
 });
-// Turn the suspendLayout flag off.
-containerPanel.suspendLayout = false;
-containerPanel.doLayout();
-containerPanel.suspendLayout = true;
 
-containerPanel.add({
-    xtype: 'panel',
+var childPanel2 = Ext.create('Ext.panel.Panel', {
     title: 'Child Panel 2',
-    height: 100,
-    columnWidth: 0.5
+    html: 'Another Panel'
 });
 
-window.setTimeout(
-    function(){
-        containerPanel.suspendLayout = false;
-        containerPanel.doLayout();
-    }
-    , 5000
-);
+Ext.create('Ext.container.Viewport', {
+    items: [ childPanel1, childPanel2]
+});
