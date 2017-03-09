39fed6da5c145379b30c84e98d751396e3cc129e
kazuemasaki <kazuemasaki@gmail.com>
2017-03-09 20:56:28 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/guide/components sample3

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -1,31 +1,10 @@
-Ext.create('Ext.tab.Panel', {
+var panel = Ext.create('Ext.panel.Panel', {
     renderTo: Ext.getBody(),
-    height: 100,
-    width: 200,
-    items: [
-        {
-            // Explicitly define the xtype of this Component configuration.
-            // This tells the Container (the tab panel in this case)
-            // to instantiate a Ext.panel.Panel when it deems necessary
-            xtype: 'panel',
-            title: 'Tab One',
-            html: 'The first tab',
-            listeners: {
-                render: function() {
-                    Ext.MessageBox.alert('Rendered One', 'Tab One was rendered.');
-                }
-            }
-        },
-        {
-            // this component configuration does not have an xtype since 'panel' is the default
-            // xtype for all Component configurations in a Container
-            title: 'Tab Two',
-            html: 'The second tab',
-            listeners: {
-                render: function() {
-                    Ext.MessageBox.alert('Rendered One', 'Tab Two was rendered.');
-                }
-            }
-        }      
-    ]
+    title: 'Test',
+    html: 'Test Panel',
+    hideMode: 'visibility' // use the CSS visibility property to show and hide this component
 });
+
+panel.hide(); // hide the component
+
+panel.show(); // show the component
