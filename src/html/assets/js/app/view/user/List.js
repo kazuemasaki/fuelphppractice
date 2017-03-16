6078cdcd1ca99a4f49f7bf932e72b4c9faf52745
kazuemasaki <kazuemasaki@gmail.com>
2017-03-16 21:01:36 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/guide/components template methods(error)

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -16,6 +16,66 @@ var panel2 = Ext.create('Ext.panel.Panel', {
     title: 'Test',
     html: 'Test Panel2',
     draggable:true,
-    shadow:false
+    shadow:false,
+    initComponent:function(){
+        console.log("initComponent");
+    },
+    beforeShow:function(){
+        console.log("beforeShow");
+    },
+    onShow:function(){
+        console.log("onShow");
+    },
+    afterShow:function(){
+        console.log("afterShow");
+    },
+    onShowComplete:function(){
+        console.log("onShowComplete");
+    },
+    afterHide:function(){
+        console.log("afterHide");
+    },
+    onRender:function(){
+        console.log("onRender");
+    },
+    afterRender:function(){
+        console.log("afterRender");
+    },
+    onEnable:function(){
+        console.log("onEnable");
+    },
+    onDisable:function(){
+        console.log("onDisable");
+    },
+    onAdded:function(){
+        console.log("onAdded");
+    },
+    onRemoved:function(){
+        console.log("onRemoved");
+    },
+    onResize:function(){
+        console.log("onResize");
+    },
+    onPosition:function(){
+        console.log("onPosition");
+    },
+    onDestroy:function(){
+        console.log("onDestroy");
+    },
+    beforeDestroy:function(){
+        console.log("beforeDestroy");
+    },
+    afterSetPosition:function(){
+        console.log("afterSetPosition");
+    },
+    afterComponentLayout:function(){
+        console.log("afterComponentLayout");
+    },
+    beforeComponentLayout:function(){
+        console.log("beforeComponentLayout");
+    }
+
 });
 panel2.show();
+panel2.center();
+
