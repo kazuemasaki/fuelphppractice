258ca1fd63c42afb0a2f744c00bd2ce05a7a8184
kazuemasaki <kazuemasaki@gmail.com>
2017-03-16 20:13:19 +0900

    http://docs.sencha.com/extjs/4.2.1/#!/guide/components sample4

--- src/html/assets/js/app/view/user/List.js
+++ src/html/assets/js/app/view/user/List.js
@@ -8,3 +8,14 @@ var panel = Ext.create('Ext.panel.Panel', {
 panel.hide(); // hide the component
 
 panel.show(); // show the component
+
+var panel2 = Ext.create('Ext.panel.Panel', {
+    width: 200,
+    height: 100,
+    floating: true, // make this panel an absolutely-positioned floating component
+    title: 'Test',
+    html: 'Test Panel2',
+    draggable:true,
+    shadow:false
+});
+panel2.show();
